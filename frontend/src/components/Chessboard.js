d4550745c9b450fc1f87baccf64ba18acf7dedfd
silkrow <si1krow@outlook.com>
2022-12-11 14:11:28 -0600

    here we got all pieces on the board

--- frontend/src/components/Chessboard.js
+++ frontend/src/components/Chessboard.js
@@ -14,7 +14,41 @@ interface Piece {
 
 const pieces: Piece[] = [];
 
+pieces.push({image:'./image/Chess_rdt60.png', x:0, y:7});
+pieces.push({image:'./image/Chess_ndt60.png', x:1, y:7});
 pieces.push({image:'./image/Chess_bdt60.png', x:2, y:7});
+pieces.push({image:'./image/Chess_qdt60.png', x:3, y:7});
+pieces.push({image:'./image/Chess_kdt60.png', x:4, y:7});
+pieces.push({image:'./image/Chess_bdt60.png', x:5, y:7});
+pieces.push({image:'./image/Chess_ndt60.png', x:6, y:7});
+pieces.push({image:'./image/Chess_rdt60.png', x:7, y:7});
+pieces.push({image:'./image/Chess_pdt60.png', x:0, y:6});
+pieces.push({image:'./image/Chess_pdt60.png', x:1, y:6});
+pieces.push({image:'./image/Chess_pdt60.png', x:2, y:6});
+pieces.push({image:'./image/Chess_pdt60.png', x:3, y:6});
+pieces.push({image:'./image/Chess_pdt60.png', x:4, y:6});
+pieces.push({image:'./image/Chess_pdt60.png', x:5, y:6});
+pieces.push({image:'./image/Chess_pdt60.png', x:6, y:6});
+pieces.push({image:'./image/Chess_pdt60.png', x:7, y:6});
+
+pieces.push({image:'./image/Chess_rlt60.png', x:0, y:0});
+pieces.push({image:'./image/Chess_nlt60.png', x:1, y:0});
+pieces.push({image:'./image/Chess_blt60.png', x:2, y:0});
+pieces.push({image:'./image/Chess_qlt60.png', x:3, y:0});
+pieces.push({image:'./image/Chess_klt60.png', x:4, y:0});
+pieces.push({image:'./image/Chess_blt60.png', x:5, y:0});
+pieces.push({image:'./image/Chess_nlt60.png', x:6, y:0});
+pieces.push({image:'./image/Chess_rlt60.png', x:7, y:0});
+pieces.push({image:'./image/Chess_plt60.png', x:0, y:1});
+pieces.push({image:'./image/Chess_plt60.png', x:1, y:1});
+pieces.push({image:'./image/Chess_plt60.png', x:2, y:1});
+pieces.push({image:'./image/Chess_plt60.png', x:3, y:1});
+pieces.push({image:'./image/Chess_plt60.png', x:4, y:1});
+pieces.push({image:'./image/Chess_plt60.png', x:5, y:1});
+pieces.push({image:'./image/Chess_plt60.png', x:6, y:1});
+pieces.push({image:'./image/Chess_plt60.png', x:7, y:1});
+
+
 
 export default function Chessboard() {
     let board = [];
